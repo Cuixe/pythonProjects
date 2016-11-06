313eb8f79f0618e6c765ac9f5a0eea04cb7ee8cf
Hugo Fuentes <hfuentes@grupobmv.com.mx>
2016-11-06 14:29:22 -0600

    Se separan las clases del views para tener un mejor control en caso de ser necesrio

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,15 +4,7 @@ from django.http import HttpResponseRedirect
 from django.views import generic
 from django.utils import timezone
 # Create your views here.
-
-
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+from view.question.IndexView import IndexView
 
 
 class DetailView(generic.DetailView):
