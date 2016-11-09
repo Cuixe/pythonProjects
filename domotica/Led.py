72beeaba3a2a84a1d56804b8dfe2d21489e58be6
Hugo Fuentes <hfuentes@grupobmv.com.mx>
2016-11-08 23:25:31 -0600

    Cambio de implementacion de leds

--- domotica/Led.py
+++ domotica/Led.py
@@ -1,12 +1,17 @@
 import RPi.GPIO as GPIO
 
+
 class Led:
-    def __init__(self, ledNumber, pin):
+
+    ledNumber = 0
+
+    def __init__(self, pin):
         self.__pin = pin
-        self.ledNumbre = ledNumber
+        self.ledNumber = Led.ledNumber
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(self.__pin, GPIO.OUT)
+        Led.ledNumber += 1
 
     def turnOn(self):
         GPIO.output(self.__pin, GPIO.HIGH)
